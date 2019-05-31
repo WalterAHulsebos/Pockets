95a126c78f0c32bff1749ac2c9bd3cfac819b5f7
Wybren van den Akker <wybren.akker@gmail.com>
2019-05-31 21:33:40 +0100

    Renamed Interfaces.

---
+++ Assets/[Source]/Scripts/Core/Interactables/ItemInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    //TODO: Add needed virtual methods in case they are needed.
+}
+
+public interface IPickupable : IInteractable
+{
+    //TODO: Add needed virtual methods in case they are needed.
+}
+
+public interface IEquipable : IInteractable
+{
+    //TODO: Add needed virtual methods in case they are needed.
+}
