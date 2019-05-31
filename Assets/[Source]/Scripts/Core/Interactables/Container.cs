f4a0ae367dfa69358c40e3d9771257131980d1c4
Unknown <jordanl8@uni.coventry.ac.uk>
2019-05-31 22:41:12 +0100

    Auto stash before merge of "Containers" and "origin/MainGameLoop"

---
+++ Assets/[Source]/Scripts/Core/Interactables/Container.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Container : MonoBehaviour
+{
+    //Room room;
+    //public List<Item> itemsInContainer
+}
