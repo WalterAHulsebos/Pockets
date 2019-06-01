2bf22f1f42998d7f51aad2228b2aeb44c54c4502
JordanL8 <jordanl8@uni.coventry.ac.uk>
2019-06-01 16:06:56 +0100

    destroy items

--- Assets/[Source]/Scripts/Core/Managers/ItemManager.cs
+++ Assets/[Source]/Scripts/Core/Managers/ItemManager.cs
@@ -90,6 +90,7 @@ public class ItemManager : PersistentSingleton<ItemManager>
                     break;
                 }
             }
+            Destroy(newItem);
         }
         CalculateFinalScore(itemCounter);
     }
