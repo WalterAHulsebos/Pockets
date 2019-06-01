36c49b79fe2d64c4b62acc37553084815b0e785b
ryan <oasis7-7@hotmail.com>
2019-06-01 20:08:01 +0100

    Request display poll game manager for item events

--- Assets/[Source]/Scripts/Core/Rooms/RequestDisplayManager.cs
+++ Assets/[Source]/Scripts/Core/Rooms/RequestDisplayManager.cs
@@ -17,6 +17,18 @@ public class RequestDisplayManager : MonoBehaviour
     void Update()
     {
         // TODO: Poll
+        ItemEvent itemEvent = GameManager.Instance.GetActiveRequestEvent();
+
+        if (itemEvent != null)
+        {
+            SetTimer(itemEvent.timeToExecute);
+
+            SetDisplays(itemEvent);
+        }
+        else
+        {
+            HideRequest();
+        }
     }
 
     void HideRequest()
@@ -34,8 +46,10 @@ public class RequestDisplayManager : MonoBehaviour
         m_TimerLabel.text = time.ToString();
     }
 
-    void SetDisplays(int itemCount)
+    void SetDisplays(ItemEvent itemEvent)
     {
+        int itemCount = itemEvent.items.Count;
+
         m_RequestDisplays[0].gameObject.SetActive(false);
         m_RequestDisplays[1].gameObject.SetActive(false);
         m_RequestDisplays[2].gameObject.SetActive(false);
@@ -43,19 +57,19 @@ public class RequestDisplayManager : MonoBehaviour
         if (itemCount > 0)
         {
             m_RequestDisplays[0].gameObject.SetActive(true);
-            m_RequestDisplays[0].SetUp("", 0);
+            m_RequestDisplays[0].SetUp(itemEvent.items[0].type, itemEvent.counts[0]);
         }
 
         if (itemCount > 1)
         {
             m_RequestDisplays[1].gameObject.SetActive(true);
-            m_RequestDisplays[1].SetUp("", 0);
+            m_RequestDisplays[1].SetUp(itemEvent.items[1].type, itemEvent.counts[1]);
         }
 
         if (itemCount > 2)
         {
             m_RequestDisplays[2].gameObject.SetActive(true);
-            m_RequestDisplays[2].SetUp("", 0);
+            m_RequestDisplays[2].SetUp(itemEvent.items[2].type, itemEvent.counts[2]);
         }
     }
 }
